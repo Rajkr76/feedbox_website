6dcbc53342361490ac8b45a4541a6afaa05dc375
Mehul Singh <mehulsingh076@gmail.com>
2025-05-26 01:07:14 +0530

    Auth structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import About from "@/components/About";
 import Events from "@/components/Events";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Login from "@/components/Login";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
+import Signup from "@/components/Signup";
 import Team from "@/components/Team";
 import Testimonials from "@/components/Testimonials";
 
@@ -12,6 +14,12 @@ export default function Home() {
   return (  
     <div>
       <Navbar/>
+
+      {/* comment out other compoennts and uncomment the one you want to test */}
+      {/* <Login/>
+      <Signup/> */}
+
+      
       <Hero/>  
       <About />
       <Services/>
